06d69728571b2c7f59b6e9d278bc5035ce38bb10
Mohammad Ali Dibaee <mohamadali.dibaee@zoodfood.com>
2020-09-17 12:35:25 +0430

    build(webpack-dev): change webpack-dev-server configs

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,12 +3,20 @@ const {merge} = require('webpack-merge')
 const WebpackBar = require('webpackbar')
 const common = require('./webpack.common.js')
 
+const PORT = 3000
+
+// eslint-disable-next-line no-console
+console.log(`Project is running at http://localhost:${PORT}`)
+
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   mode: 'development',
   devServer: {
     hot: true,
+    port: PORT,
     noInfo: true,
+    compress: true,
+    historyApiFallback: true,
     clientLogLevel: 'silent',
     contentBase: path.join(process.cwd(), 'dist'),
   },
