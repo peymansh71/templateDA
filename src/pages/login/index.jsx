7bb61aac58fa9a8af2ed9a3ced9c0190092ce2aa
Mohammad Ali Dibaee <mohamadali.dibaee@zoodfood.com>
2020-09-24 00:47:52 +0330

    feat(login): add login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
+import {Helmet} from 'react-helmet'
+import {Link} from 'react-router-dom'
+import {Button, Card} from 'reactstrap'
+import {useTranslation} from 'react-i18next'
 
-const Login = () => (
-  <>
-    <div>Login</div>
-  </>
-)
+import {validateEmail} from '~utils'
+
+import FormInput from '~components/form/Input'
+import FormPassword from '~components/form/Password'
+
+import logo from '~assets/images/logo/DA_logo_color.png'
+
+import s from './styles.m.scss'
+
+const initialState = {email: null, password: null}
+
+const Login = () => {
+  const {t} = useTranslation()
+
+  const formRef = useRef(null)
+
+  const [validity, setValidity] = useState(initialState)
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+    const data = new FormData(formRef.current)
+
+    const state = {...initialState}
+
+    if (!validateEmail(data.get('e-mail'))) state.email = false
+    else state.email = true
+
+    if (!data.get('password')) state.password = false
+    else state.password = true
+
+    const status = Object.values(state).reduce((a, b) => a && b)
+
+    if (status) {
+      setValidity(initialState)
+      return data
+    }
+
+    setValidity(state)
+    return false
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>Diversity Atlas|Login</title>
+      </Helmet>
+      <div className={s.login}>
+        <Card className='px-4 pt-4 pb-3'>
+          <div className='d-flex justify-content-center mb-4'>
+            <img alt='diversity-logo' src={logo} className={s.login__logo} />
+          </div>
+          <form ref={formRef}>
+            <FormInput
+              className={s.login__input}
+              name='e-mail'
+              label=''
+              validity={validity.email}
+              placeholder='johndoe@mail.com'
+            />
+            <FormPassword
+              className={s.login__input}
+              validity={validity.password}
+              placeholder='password'
+              label=''
+            />
+            <Button
+              id='submit-btn'
+              className={s.login__submit}
+              color='secondary'
+              block
+              onClick={onSubmit}
+            >
+              {t('login')}
+            </Button>
+            <div className={s.login__links}>
+              <Link to='/forgot-password'>{t('forget_password')}</Link>
+              <div>
+                <span>{t('i_dont_have_an_account')}</span>
+                <Link to='/registration'> {t('register')}.</Link>
+              </div>
+            </div>
+            <hr className={s.login__divider} />
+            <div className='d-flex justify-content-center'>
+              <Link to='/contact-us'>{t('contact_us')}</Link>
+            </div>
+          </form>
+        </Card>
+      </div>
+    </>
+  )
+}
 
 export default Login
